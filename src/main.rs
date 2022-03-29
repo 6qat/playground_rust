c7361adb24bf3956a723b3cc6cb52877a05b9c23
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-03-29 11:59:44 -0300

    Just playing.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_assignments)]
 #[allow(unused_variables)]
+#[allow(dead_code)]
 
 fn main() {
     let r;
@@ -10,9 +11,16 @@ fn main() {
     }
     //assert_eq!(*r, 1);
 
-    let v = vec![1,2,3];
+    let v = vec![1, 2, 3];
     let r = &v[1];
     assert_eq!(*r, 2);
 
+    static mut STASH: &i32 = &0;
 
+    fn f(p: &'static i32) {
+        unsafe {
+            STASH = p;
+        }
+
+    }
 }
