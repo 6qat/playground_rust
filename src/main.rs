b65fc53551c6052f95ae34776f1d2ec71534632b
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-03-29 11:49:12 -0300

    Just playing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[allow(unused_assignments)]
+#[allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    let r;
+    {
+        let x = 1;
+        r = &x;
+        assert_eq!(*r, 1);
+    }
+    //assert_eq!(*r, 1);
+
+    let v = vec![1,2,3];
+    let r = &v[1];
+    assert_eq!(*r, 2);
+
+
 }
