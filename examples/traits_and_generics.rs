96ee05a7ed29814812412cab2ae6a85fe20009b9
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-04-01 06:56:34 -0300

    Playing with pattern match!

--- examples/traits_and_generics.rs
+++ examples/traits_and_generics.rs
@@ -1,18 +1,29 @@
+use std::any::TypeId;
 use std::io::Write;
 use std::fs::File;
 use std::io::Result;
 
-fn say_hello(out: &mut dyn Write) -> Result<()>  {
+fn _say_hello(out: &mut dyn Write) -> Result<()> {
+    if TypeId::of::<*mut dyn Write>() == TypeId::of::<File>() {
+        println!("File");
+    }
     out.write_all(b"hello world\n")?;
     out.flush()
 }
 
+fn _say_hello2<W: 'static + Write>(_out: &mut W) -> Result<()> {
+    if TypeId::of::<W>() == TypeId::of::<File>() {
+        println!("File");
+    }
+    _out.write_all(b"hello world\n")?;
+    _out.flush()
+}
+
 fn main() {
     let mut local_file = File::create("hello.txt").unwrap();
-    say_hello(&mut local_file).unwrap();
+    _say_hello2(&mut local_file).unwrap();
 
     let mut bytes = vec![];
-    say_hello(&mut bytes).unwrap();
+    _say_hello2(&mut bytes).unwrap();
     assert_eq!(bytes, b"hello world\n");
-
 }
