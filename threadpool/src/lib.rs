8f800035ebfca690691c81d9de0d8831e62334ed
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-08-18 21:46:44 -0300

    Correct dependencies in lab project.

--- threadpool/src/lib.rs
+++ threadpool/src/lib.rs
@@ -1,10 +1,39 @@
 #![allow(dead_code, unused)]
+#![allow(clippy::empty_loop)]
 
-pub struct ThreadPool;
+use std::sync::Mutex;
+use std::sync::{mpsc, Arc};
+
+pub struct ThreadPool {
+    _handles: Vec<std::thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
-    pub fn new(size: usize) -> Self {
-        Self
+    pub fn new(num_threads: u8) -> Self {
+        let (sender, receiver) = mpsc::channel::<Box<dyn Fn() + Send>>();
+        let receiver = Mutex::new(receiver);
+
+        // let handles = (0..num_threads)
+        //     .map(|_| {
+        //         std::thread::spawn(move || loop {
+        //             let work = receiver.lock().unwrap().recv().unwrap();
+        //             work();
+        //         })
+        //     })
+        //     .collect();
+
+        let receiver = Arc::new(receiver);
+        let mut handles = Vec::new();
+        for _ in 0..num_threads {
+            let clone = receiver.clone();
+            let handle = std::thread::spawn(move || loop {
+                let work = clone.lock().unwrap().recv().unwrap();
+                work();
+            });
+            handles.push(handle);
+        }
+
+        Self { _handles: handles }
     }
 
     pub fn execute<T: Fn()>(&self, work: T) {
