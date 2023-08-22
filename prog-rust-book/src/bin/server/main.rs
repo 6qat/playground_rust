38de997a1e772cc44d6666a23994a58d8f81fd3f
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-08-22 14:09:17 -0300

    Complete code from author repo (https://github.com/ProgrammingRust/async-chat)

--- prog-rust-book/src/bin/server/main.rs
+++ prog-rust-book/src/bin/server/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+use async_std::prelude::*;
+
+use connection::serve;
+use prog_rust_book::utils::ChatResult;
+
+mod connection;
+mod group;
+mod group_table;
+
+fn main() -> ChatResult<()> {
+    let address = std::env::args().nth(1).expect("Usage: server ADDRESS");
+
+    let chat_group_table = Arc::new(group_table::GroupTable::new());
+
+    async_std::task::block_on(async {
+        // This code was shown in the chapter introduction.
+        use async_std::{net, task};
+
+        let listener = net::TcpListener::bind(address).await?;
+
+        let mut new_connections = listener.incoming();
+        while let Some(socket_result) = new_connections.next().await {
+            let socket = socket_result?;
+            let groups = chat_group_table.clone();
+            task::spawn(async {
+                log_error(serve(socket, groups).await);
+            });
+        }
+
+        Ok(())
+    })
+}
+
+fn log_error(result: ChatResult<()>) {
+    if let Err(error) = result {
+        eprintln!("Error: {}", error);
+    }
 }
