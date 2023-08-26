3d88f6c6ed5733457a5ac68a81878308f4bd3e4b
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-08-25 21:01:23 -0300

    Refactoring

--- lab/examples/async-await.rs
+++ lab/examples/async-await.rs
@@ -1,4 +1,5 @@
 use futures::executor::block_on;
+use futures::join;
 
 async fn task1() -> i32 {
     println!("First greeter");
@@ -13,7 +14,7 @@ async fn task2() -> i32 {
 async fn async_main() {
     // greeter().await;
     // second_greet().await;
-    let (result1, result2) = futures::join!(task1(), task2());
+    let (result1, result2) = join!(task1(), task2());
     println!("{} {}", result1, result2);
 }
 
